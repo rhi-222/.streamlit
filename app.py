53405ffce97ee884c2eec4c1fa2c1ef875e0f7c0
Rhi <rhi.222@icloud.com>
2024-06-17 21:47:04 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import plotly.express as px
 # Load the dataset
 df = pd.read_csv(r'/Users/rhiannonfillingham/PROJECT4/vehicles_us.csv')
 
-# Header
+# Streamlit headers and components
 st.header('Exploratory Data Analysis of Vehicles Dataset')
 
 # Display the first few rows of the dataset
@@ -14,10 +14,15 @@ if st.checkbox('Show raw data'):
 
 # Plotly Express histogram
 st.header('Histogram of Vehicle Prices')
-fig_hist = px.histogram(df, x='price', title='Histogram of Vehicle Prices')
-st.plotly_chart(fig_hist)
+fig_price_hist = px.histogram(df, x='price', title='Histogram of Vehicle Prices')
+st.plotly_chart(fig_price_hist)
 
 # Plotly Express scatter plot
 st.header('Scatter Plot of Price vs Mileage')
-fig_scatter = px.scatter(df, x='price', y='odometer', title='Scatter Plot of Price vs Mileage')
-st.plotly_chart(fig_scatter)
+fig_price_vs_mileage = px.scatter(df, x='odometer', y='price', title='Scatter Plot of Price vs Mileage')
+st.plotly_chart(fig_price_vs_mileage)
+
+# Scatter plot of price vs model year
+st.header('Scatter Plot of Price vs Model Year')
+fig_price_vs_year = px.scatter(df, x='model_year', y='price', title='Scatter Plot of Price vs Model Year')
+st.plotly_chart(fig_price_vs_year)
